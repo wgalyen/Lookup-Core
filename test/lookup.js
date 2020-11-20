ec80db7b6da83cde41246e24557bbe1b7d6ab3cd
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-20 08:44:19 -0800

    YAML support for meta information

--- test/lookup.js
+++ test/lookup.js
@@ -1,13 +1,18 @@
+
+'use strict';
+
 /*jshint expr: true*/
 
-var chai = require('chai'),
-    should = chai.should(),
-    expect = chai.expect,
-    lookup = require('../lib/lookup');
+// Modules
+var chai   = require('chai');
+var expect = chai.expect;
+var lookup = require('../lib/lookup.js');
 
+chai.should();
 chai.config.truncateThreshold = 0;
 
 describe('#cleanString()', function () {
+
     it('converts "Hello World" into "hello-world"', function () {
         lookup.cleanString('Hello World').should.equal('hello-world');
     });
@@ -27,9 +32,11 @@ describe('#cleanString()', function () {
     it('converts "/some/directory-example/underscores/" into "some_directory_example_underscores"', function () {
         lookup.cleanString('/some/directory-example/underscores/', true).should.equal('some_directory_example_underscores');
     });
+
 });
 
 describe('#slugToTitle()', function () {
+
     it('converts "hello-world" into "Hello World"', function () {
         lookup.slugToTitle('hello-world').should.equal('Hello World');
     });
@@ -37,70 +44,101 @@ describe('#slugToTitle()', function () {
     it('converts "dir/some-example-file.md" into "Some Example File"', function () {
         lookup.slugToTitle('dir/some-example-file.md').should.equal('Some Example File');
     });
+
 });
 
 describe('#processMeta()', function () {
+
     it('returns array of meta values', function () {
-        var result = lookup.processMeta('/*\n' +
-            'Title: This is a title\n' +
-            'Description: This is a description\n' +
-            'Sort: 4\n' +
-            'Multi word: Value\n' +
+        var result = lookup.processMeta('/*\n'+
+            'Title: This is a title\n'+
+            'Description: This is a description\n'+
+            'Sort: 4\n'+
+            'Multi word: Value\n'+
             '*/\n');
         expect(result).to.have.property('title', 'This is a title');
         expect(result).to.have.property('description', 'This is a description');
         expect(result).to.have.property('sort', '4');
         expect(result).to.have.property('multi_word', 'Value');
     });
+
     it('returns an empty array if no meta specified', function () {
         var result = lookup.processMeta('no meta here');
         expect(result).to.be.empty;
     });
+
     it('returns proper meta from file starting with a BOM character', function () {
-        looup.config.content_dir = __dirname + '/content/';
-        var result = looup.getPage(looup.config.content_dir + 'page-with-bom.md');
+        lookup.config.content_dir = __dirname + '/content/';
+        var result = lookup.getPage(lookup.config.content_dir + 'page-with-bom.md');
         expect(result).to.have.property('title', 'Example Page With BOM');
     });
+
+    it('returns array of meta values (YAML)', function () {
+        var result = lookup.processMeta('\n'+
+            'Title: This is a title\n'+
+            'Description: This is a description\n'+
+            'Sort: 4\n'+
+            'Multi word: Value\n'+
+            '---\n');
+        expect(result).to.have.property('title', 'This is a title');
+        expect(result).to.have.property('description', 'This is a description');
+        expect(result).to.have.property('sort', '4');
+        expect(result).to.have.property('multi_word', 'Value');
+    });
+
+    it('returns proper meta from file starting with a BOM character (YAML)', function () {
+        lookup.config.content_dir = __dirname + '/content/';
+        var result = lookup.getPage(lookup.config.content_dir + 'page-with-bom-yaml.md');
+        expect(result).to.have.property('title', 'Example Page With BOM for YAML');
+    });
+
 });
 
 describe('#stripMeta()', function () {
+
     it('strips meta comment block', function () {
-        var result = lookup.stripMeta('/*\n' +
-            'Title: This is a title\n' +
-            'Description: This is a description\n' +
-            'Sort: 4\n' +
-            'Multi word: Value\n' +
+        var result = lookup.stripMeta('/*\n'+
+            'Title: This is a title\n'+
+            'Description: This is a description\n'+
+            'Sort: 4\n'+
+            'Multi word: Value\n'+
             '*/\nThis is the content');
         result.should.equal('This is the content');
     });
+
     it('leaves content if no meta comment block', function () {
         var result = lookup.stripMeta('This is the content');
         result.should.equal('This is the content');
     });
+
     it('only strips the first comment block', function () {
-        var result = lookup.stripMeta('/*\n' +
-            'Title: This is a title\n' +
-            'Description: This is a description\n' +
-            'Sort: 4\n' +
-            'Multi word: Value\n' +
-            '*/\nThis is the content/*\n' +
-            'Title: This is a title\n' +
+        var result = lookup.stripMeta('/*\n'+
+            'Title: This is a title\n'+
+            'Description: This is a description\n'+
+            'Sort: 4\n'+
+            'Multi word: Value\n'+
+            '*/\nThis is the content/*\n'+
+            'Title: This is a title\n'+
             '*/');
-        result.should.equal('This is the content/*\n' +
-            'Title: This is a title\n' +
+        result.should.equal('This is the content/*\n'+
+            'Title: This is a title\n'+
             '*/');
     });
+
 });
 
 describe('#processVars()', function () {
+
     it('replaces config vars in Markdown content', function () {
         lookup.config.base_url = '/base/url';
         lookup.processVars('This is some Markdown with a %base_url%.')
             .should.equal('This is some Markdown with a /base/url.');
     });
+
 });
 
 describe('#getPage()', function () {
+
     it('returns an array of values for a given page', function () {
         lookup.config.content_dir = __dirname + '/content/';
         var result = lookup.getPage(lookup.config.content_dir + 'example-page.md');
@@ -109,14 +147,17 @@ describe('#getPage()', function () {
         expect(result).to.have.property('body');
         expect(result).to.have.property('excerpt');
     });
+
     it('returns null if no page found', function () {
         lookup.config.content_dir = __dirname + '/content/';
         var result = lookup.getPage(lookup.config.content_dir + 'nonexistent-page.md');
         expect(result).to.be.null;
     });
+
 });
 
 describe('#getPages()', function () {
+
     it('returns an array of categories and pages', function () {
         lookup.config.content_dir = __dirname + '/content/';
         var result = lookup.getPages();
@@ -125,23 +166,28 @@ describe('#getPages()', function () {
         expect(result[1]).to.have.property('slug', 'sub');
         expect(result[1].files[0]).to.have.property('title', 'Example Sub Page');
     });
+
     it('marks activePageSlug as active', function () {
         lookup.config.content_dir = __dirname + '/content/';
         var result = lookup.getPages('/example-page');
         expect(result[0].files[0]).to.have.property('active', true);
         expect(result[1].files[0]).to.have.property('active', false);
     });
+
 });
 
 describe('#doSearch()', function () {
+
     it('returns an array of search results', function () {
         lookup.config.content_dir = __dirname + '/content/';
         var result = lookup.doSearch('example');
-        expect(result).to.have.length(3);
+        expect(result).to.have.length(4);
     });
+
     it('returns an empty array if nothing found', function () {
         lookup.config.content_dir = __dirname + '/content/';
         var result = lookup.doSearch('asdasdasd');
         expect(result).to.be.empty;
     });
+
 });
